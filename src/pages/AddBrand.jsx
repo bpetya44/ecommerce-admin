177cc08e076199cd0181d63de008d6cd09ee449a
Petya Angelova <bedfordpetya@gmail.com>
2023-03-19 14:06:51 +0200

    connected to backend, form validation, toastify added

--- src/pages/AddBrand.jsx
+++ src/pages/AddBrand.jsx
@@ -1,13 +1,67 @@
-import React from "react";
+import { useEffect } from "react";
 import CustomInput from "../components/CustomInput";
+import { useDispatch, useSelector } from "react-redux";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { createBrand } from "../features/brand/brandSlice";
+
+//Yup schema
+let schema = yup.object({
+  title: yup.string().required("Brand name is required"),
+});
 
 const AddBrand = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const newBrand = useSelector((state) => state.brand);
+  const { isLoading, isSuccess, isError, createdBrand } = newBrand;
+
+  useEffect(() => {
+    if (isSuccess && createdBrand) {
+      toast.success("Brand Added Successfully!");
+    }
+    if (isError) {
+      toast.error("Something went wrong!");
+    }
+  }, [isLoading, isSuccess, isError, createdBrand]);
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+    },
+    validationSchema: schema,
+
+    onSubmit: (values) => {
+      //console.log(values);
+      dispatch(createBrand(values));
+      //alert(JSON.stringify(values));
+      formik.resetForm();
+
+      setTimeout(() => {
+        navigate("/admin/brand-list");
+      }, 3000);
+    },
+  });
+
   return (
     <div>
       <h3 className="mb-4 title">Add Brand</h3>
       <div>
-        <form action="">
-          <CustomInput type="text" label="Enter Brand" />
+        <form action="" onSubmit={formik.handleSubmit}>
+          {/* Title */}
+          <CustomInput
+            type="text"
+            label="Enter Brand"
+            name="title"
+            onCh={formik.handleChange("title")}
+            onBl={formik.handleBlur("title")}
+            val={formik.values.title}
+          />
+          <div className="error">
+            {formik.touched.title && formik.errors.title}
+          </div>
           <button className="btn btn-primary border-0 rounded-3 my-4">
             Add Brand
           </button>
--- src/pages/AddCategory.jsx
+++ src/pages/AddCategory.jsx
@@ -1,13 +1,66 @@
-import React from "react";
+import { useEffect } from "react";
 import CustomInput from "../components/CustomInput";
+import { useDispatch, useSelector } from "react-redux";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { createCategory } from "../features/category/categorySlice";
+
+//Yup schema
+let schema = yup.object({
+  title: yup.string().required("Category name is required"),
+});
 
 const AddCategory = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const newCategory = useSelector((state) => state.category);
+  const { isLoading, isSuccess, isError, createdCategory } = newCategory;
+
+  useEffect(() => {
+    if (isSuccess && createdCategory) {
+      toast.success("Category is added Successfully!");
+    }
+    if (isError) {
+      toast.error("Something went wrong!");
+    }
+  }, [isLoading, isSuccess, isError, createdCategory]);
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      //console.log(values);
+      dispatch(createCategory(values));
+      //alert(JSON.stringify(values));
+      formik.resetForm();
+
+      setTimeout(() => {
+        navigate("/admin/category-list");
+      }, 3000);
+    },
+  });
+
   return (
     <div>
       <h3 className="mb-4 title">Add Category</h3>
       <div>
-        <form action="">
-          <CustomInput type="text" label="Enter Category" />
+        <form action="" onSubmit={formik.handleSubmit}>
+          {/* Title */}
+          <CustomInput
+            type="text"
+            label="Enter Category"
+            name="title"
+            onCh={formik.handleChange("title")}
+            onBl={formik.handleBlur("title")}
+            val={formik.values.title}
+          />
+          <div className="error">
+            {formik.touched.title && formik.errors.title}
+          </div>
           <button className="btn btn-primary border-0 rounded-3 my-4">
             Add Category
           </button>
